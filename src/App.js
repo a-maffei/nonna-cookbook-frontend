8bb5d778213449de4cc4eb682b317cb6853dc9d0
Macanijus <tmacan975@gmail.com>
2022-11-23 11:08:01 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
+import { client } from "./client";
 
 function App() {
-  return <div className="App"></div>;
+  const [recipes, setRecipes] = useState([]);
+
+  function fetchRecipes() {
+    client
+      .getEntries()
+      .then((res) => {
+        console.log(res.items.fields);
+        setRecipes(res.fields);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => fetchRecipes, []);
+
+  console.log(recipes);
+
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/client.js
@@ -0,0 +1,5 @@
+import * as contentful from "contentful";
+export const client = contentful.createClient({
+  space: "2w4zg33ce5gj",
+  accessToken: "wEOclvfUK7YefxiEFgPbVkMUHJHg7eU3I5hwstTGhEI",
+});
