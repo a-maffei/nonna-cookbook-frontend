31e2b6250ee7e2e94948082c33baae6234b79742
Alessandra Maffei <ales.maffei@gmail.com>
2022-11-24 08:42:14 +0100

    more styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./footer.css";
+import { FaPizzaSlice } from "react-icons/fa";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <p className="footer-text">
+        Made with hunger by Alessandra, Antonio and Armin{" "}
+        <FaPizzaSlice className="footer-icon" />
+      </p>
+    </div>
+  );
 }
