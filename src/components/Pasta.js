fbe8b2ec231e9450c29c7035850655004f04b030
Alessandra Maffei <ales.maffei@gmail.com>
2022-11-24 06:49:27 +0100

    more styling

--- src/components/Pasta.js
+++ src/components/Pasta.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./category.css";
+
 export default function Pasta({ pasta }) {
   return (
     <div>
-      Pasta
+      <h1 className="category-title">Pasta</h1>
       {pasta &&
         pasta.map((recipe, key) => (
           <div>
--- src/components/Starters.js
+++ src/components/Starters.js
@@ -1,25 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Startersrecipe from "./Startersrecipe";
+import "./category.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Starters({ starters }) {
   console.log(starters);
+  const navigate = useNavigate();
 
   return (
     <div>
-      Starters
-      {starters &&
-        starters.map((recipe, key) => (
-          <div>
-            <Link to={`/starters/${recipe.fields.nameId}`}>
-              <h3> {recipe.fields.title} </h3>
-            </Link>
-            <img
-              src={recipe.fields.image.fields.file.url}
-              style={{ width: "100px" }}
-            />
-          </div>
-        ))}
+      <h2 className="category-title">Starters</h2>
+      <p>Blababla.</p>
+      <div className="category-container">
+        {starters &&
+          starters.map((recipe, key) => (
+            <div className="category-recipe-container">
+              <Link
+                to={`/starters/${recipe.fields.nameId}`}
+                style={{ textDecoration: "none" }}
+              >
+                <h3 className="category-recipe-title">
+                  {" "}
+                  {recipe.fields.title}{" "}
+                </h3>
+
+                <img
+                  src={recipe.fields.image.fields.file.url}
+                  className="category-recipe-img"
+                />
+              </Link>
+            </div>
+          ))}
+      </div>
+      <div className="bttn-container">
+        <h4 className="bttn-intro">Not quite what you were looking for?</h4>
+        <button className="nav-bttn" onClick={() => navigate("/")}>
+          Go back to all recipes
+        </button>
+      </div>
     </div>
   );
 }
